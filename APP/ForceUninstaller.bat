45823dfb585df74d5cff2702a1897a4c979ab6f6
Holy Cheese Man <lightwhoosh@gmail.com>
2024-09-23 21:16:30 +1200

    Delete APP/ForceUninstaller.bat

--- APP/ForceUninstaller.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScriptingPlus\APP"
-rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScriptingPlus\APP\Template"
