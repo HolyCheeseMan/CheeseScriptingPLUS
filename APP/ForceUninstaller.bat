3a369fb0b9df964a70647c27b545574af3ee3802
Holy Cheese Man <lightwhoosh@gmail.com>
2024-09-23 20:57:35 +1200

    Add files via upload

---
+++ APP/ForceUninstaller.bat
@@ -0,0 +1,3 @@
+@echo off
+rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScriptingPlus\APP"
+rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\HolyCheeseMan\CheeseScriptingPlus\APP\Template"
